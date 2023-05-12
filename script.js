c75fb4765d4dcd01d64d4e2db1036dd910734632
Kabir Abdulkareem <kabirabdulkareem@gmail.com>
2023-05-12 19:17:00 +0100

    completed

---
+++ script.js
@@ -0,0 +1,6 @@
+const learning = document.querySelector("#learning");
+const update = document.querySelector("#update");
+
+update.addEventListener("click", () => {
+  learning.innerHTML = "I am currently learning Javascript";
+});
